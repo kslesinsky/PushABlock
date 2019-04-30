2a511c4851f354b1f5c87c14952f9e9c17b02ae4
Kevin Slesinsky <info@kevinvictor.com>
2019-04-30 15:03:39 -0400

    level dropdown & play button

--- Assets/KevinAssets/MyScripts/MainEngine.cs
+++ Assets/KevinAssets/MyScripts/MainEngine.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainEngine : MonoBehaviour
 {
     public static int Level;
+
+    Dropdown ddLevel;
+
+    public void Start()
+    {
+        ddLevel = GameObject.Find("ddLevel").GetComponent<Dropdown>();
+    }
+
+    public void Play()
+    {
+        int level = ddLevel.value;
+        PlayLevel(level);
+    }
 
     public void PlayLevel(int level)
     {
